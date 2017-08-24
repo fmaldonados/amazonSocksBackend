af5f8b1e6e9642b968487d148e4f682d0198f39d
Felix Maldonado <fmaldonado@hnsc.net>
2017-08-24 13:23:58 -0600

    creando archivos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var hapi = require('hapi');
 var inert = require('inert');
 var mongoose = require('mongoose');
 var routes = require('./routes');
-var auth = require('hapi-auth-cookie');
+//var auth = require('hapi-auth-cookie');
 
 var server = new hapi.Server();
 server.connection({
@@ -10,7 +10,7 @@ server.connection({
     routes: {cors: true}
 });
 
-mongoose.connect('mongodb://zsnake:password@ds025772.mlab.com:25772/angular-scaffold');
+mongoose.connect('mongodb://localhost:27017/nombre_bd');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
@@ -18,17 +18,22 @@ db.once('open', function callback() {
     console.log("Connection with database succeeded.");
 });
 
-server.register([inert, auth], function(err){
+server.register([inert], function(err){
 
-  server.auth.strategy('session', 'cookie', {
-    password: 'secretpasswordforencryption',
-    cookie: 'angular-scaffold-cookie',
-    ttl: 24 * 60 * 60 * 1000, // Set session to 1 day
-    isSecure: false
-  });
+//   server.auth.strategy('session', 'cookie', {
+//     password: 'secretpasswordforencryption',
+//     cookie: 'angular-scaffold-cookie',
+//    ttl: 24 * 60 * 60 * 1000, // Set session to 1 day
+//     isSecure: false
+//   });
 
-	server.route(routes.endpoints);
-	server.start(function () {
-	    console.log('Server running at:', server.info.uri);
+    server.route(routes.endpoints);
+
+     //server.route([{method:"GET", path:"/",config:
+     //{handler:function(request,reply){
+       //  return reply("ok");
+     //}}}]);
+	 server.start(function () {
+	     console.log('Server running at:', server.info.uri);
 	});
 });
